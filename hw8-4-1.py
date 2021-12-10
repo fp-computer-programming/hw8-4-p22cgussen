296dc0ea799eca42fbaa7c17e047e643ac4d95a7
p22cgussen <p22cgussen@fairfieldprep.org>
2021-12-09 22:05:43 -0500

    complete hw8-4

---
+++ hw8-4-1.py
@@ -0,0 +1,20 @@
+# Charlie Gussen: CCG 12/9/21
+
+import random
+
+number = random.randint(0, 50)
+
+while True:
+    user_number = input("Guess a number from 0-50. ")
+    if user_number == "stop":
+        print("The random number was " + str(number))
+        break
+    if int(user_number) > number:
+        print("Guess lower")
+        continue
+    elif int(user_number) < number:
+        print("Guess higher")
+        continue
+    elif int(user_number) == number:
+        print("You guessed the correct number!")
+        break
